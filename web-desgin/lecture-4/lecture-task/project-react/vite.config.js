5c81c4eecaac97eab88e50766c6f6f452325101b
Rasha jouda <rashajouda21@gmail.com>
2025-11-02 01:35:09 +0300

    change namea

--- web-desgin/lecture-4/lecture-task/project-react/vite.config.js
+++ web-desgin/lecture-4/lecture-task/project-react/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   base: '/Acadimic-assignments/web%20design%20and%20development/lecture-4/lecture-task/sample-project-using-react/',
