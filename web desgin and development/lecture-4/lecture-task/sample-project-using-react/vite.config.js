f06cba6984e96459f773803ca8debf14bd4595b8
Rasha jouda <rashajouda21@gmail.com>
2025-11-02 01:21:10 +0300

    Update vite.config.js

--- web desgin and development/lecture-4/lecture-task/sample-project-using-react/vite.config.js
+++ web desgin and development/lecture-4/lecture-task/sample-project-using-react/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
+  base: 'Acadimic-assignments/web desgin and development/lecture-4/lecture-task/sample-project-using-react', 
 })
